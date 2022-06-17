12161e99cfe2e669b00ac300a4735e99c9db6230
Martin P <mart803d@edu.zealand.dk>
2022-06-17 11:23:29 +0200

    Fjernet SoMe Links fra Nav (% Hopscreek)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
+// Burger menu https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
  function burgerFunction() {
      var x = document.querySelector('#dropdown');
      if (x.style.display === "block") {
@@ -9,7 +9,7 @@
    }
 
 
-  //  https://stackoverflow.com/questions/11679567/using-css-for-a-fade-in-effect-on-page-load - metode 3
+  // Fade in  https://stackoverflow.com/questions/11679567/using-css-for-a-fade-in-effect-on-page-load - metode 3
    $("header").delay(1000).animate({"opacity": "1"}, 500);
    $("section").delay(1000).animate({"opacity": "1"}, 500);
    $("div").delay(1000).animate({"opacity": "1"}, 1600);
