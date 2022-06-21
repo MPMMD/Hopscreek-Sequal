cb7c45f6c580858e1f9e00bd6a7b4498f2b3e3b3
Martin P <mart803d@edu.zealand.dk>
2022-06-21 17:16:57 +0200

    omg koncert siden virker nu, javascript

--- main.js
+++ main.js
@@ -8,8 +8,32 @@
      }
    }
 
-
   // Fade in  https://stackoverflow.com/questions/11679567/using-css-for-a-fade-in-effect-on-page-load - metode 3
    $("header").delay(1000).animate({"opacity": "1"}, 500);
    $("section").delay(1000).animate({"opacity": "1"}, 500);
    $("div").delay(1000).animate({"opacity": "1"}, 1600);  
+
+  // toggle  https://codepen.io/fahim04blue/pen/KKNvOZe
+   const julibtn = document.querySelector('#juli');
+   const junibtn = document.querySelector('#juni');
+   const juni = document.querySelectorAll('.ticket-juni')
+   const juli = document.querySelectorAll('.ticket-juli')
+
+
+   julibtn.addEventListener("click", function(){
+    juni.forEach(junimonth=>{
+      junimonth.style.display = "none";
+    })   
+    juli.forEach(julimonth=>{
+      julimonth.style.display = "block";
+    })   
+   });
+
+   junibtn.addEventListener("click", function(){
+    juni.forEach(junimonth=>{
+      junimonth.style.display = "block";
+    })   
+    juli.forEach(julimonth=>{
+      julimonth.style.display = "none";
+    })         
+ });
